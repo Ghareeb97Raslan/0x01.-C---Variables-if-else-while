79ccc3966fa72f0f944acbfb1d87a2b6ef361386
Ghareeb97Raslan <96418076+Ghareeb97Raslan@users.noreply.github.com>
2023-07-14 01:55:19 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
-#include <stdio>
+#include <stdio.h>
 /**
- * mine -Entry point
- *
- * Return Always 0(Success)
- */ int maine (void)
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
